0cca735c871551050349dc1e5d1e9b37257b9023
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-10-26 20:00:53 +0000

    morew ork

--- src/component/container/index.js
+++ src/component/container/index.js
@@ -1,36 +1,67 @@
 import React from 'react';
 import Form from '../form';
 
+import uuid from 'uuid/v4';
+
+const initialPair = {
+  id: uuid(),
+  key: 'yes',
+  value: 'poop',
+};
+
+
+
 class Container extends React.Component{
   constructor(props){
     super(props);
 
     this.state = {
-      keyValArray: [],
-    }
+      keyValArray: [initialPair],
+    };
 
-    this.addInput = this.addInput.bind(this);
     this.updateInput = this.updateInput.bind(this);
+    this.addInput = this.addInput.bind(this);
   }
-
+  
+  componentDidUpdate(){
+    console.log('__STATE__', this.state.keyValArray);
+  }
+  
   addInput(){
-    count++;
+    const newPair = {
+      id: uuid(),
+      key: '',
+      value: '',
+    };
+    this.setState(prevState => ({
+      keyValArray: [...prevState.keyValArray, newPair],
+    }));
   }
   
   updateInput(input){
-    this.setState()
+    console.log(input);
   }
 
 
 
   render(){
+    let {keyValArray} = this.state;
+    // console.log(keyValArray[0]);
     return(
       <div className='container'>
         <h1> This is the Containter </h1>
         <div onClick={this.addInput}> + </div>
-        <Form updateInput={this.updateInput}  />
+        
+        {keyValArray.map((item,i) => (
+          <Form 
+            data={item}
+            updateInput={this.updateInput}
+            key={i}
+          />
+        ))}
+
       </div>
-    )
+    );
   }
 };
 
--- src/component/form/index.js
+++ src/component/form/index.js
@@ -1,45 +1,53 @@
 import React from 'react';
 
-const emptyState = {
-  key: '',
-  value: '',
-};
-
 class Form extends React.Component {
   constructor(props){
     super(props);
 
-    this.state = emptyState;
+    this.state = this.props.data;
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+  
+  componentDidMount(){
+    console.log(this.state);
+  }
+  
+  handleBlur(){
+    console.log('pre', this.state);
+    this.props.updateInput(this.state);
   }
 
   handleChange(e){
     let {name, value} = e.target;
     this.setState({
-      [name]: value
-    })
-
-    this.props.updateInput(this.state, this.props.index);
+      [name]: value,
+    });
   }
 
   render(){
+    // console.log('form id', this.props.id);
     return(
-      <form>
-        <input
-          type='text'
-          name='key'
-          placeholder='key'
-          onChange={this.handleChange}
-        />
-        <input
-          type='text'
-          name='value'
-          placeholder='value'
-          onChange={this.handleChange}
-        />
-      </form>
-    )
+      <div onBlur={this.handleBlur}>
+        <form>
+          <input
+            type='text'
+            name='key'
+            placeholder='key'
+            onChange={this.handleChange}
+            value={this.state.key}
+          />
+          <input
+            type='text'
+            name='value'
+            placeholder='value'
+            onChange={this.handleChange}
+            value={this.state.value}
+          />
+        </form>
+      </div>
+    );
   }
 
 
